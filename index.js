7881083ac394de036604e5cde7106b708245c8aa
Mike <mike@myhometouch.com>
2017-05-15 22:39:35 +0100

    Amended index to defer http client config to xboxApiClient and pass through to all modules as required.

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 module.exports = (xboxidApiToken) => {
-    const axios = require('axios').create({
-        baseURL: 'https://xboxapi.com/v2',
-        headers: { 'X-AUTH': xboxidApiToken }
-    });
+    const xboxApiClient = require('./lib/xboxApiClient.js')(xboxidApiToken);
 
     return Object.assign(
         {},
-        require('./lib/self.js')(axios),
-        require('./lib/user.js')(axios),
-        require('./lib/media.js')(axios),
-        require('./lib/clubs.js')(axios),
-        require('./lib/games.js')(axios),
-        require('./lib/marketplace.js')(axios));
+        require('./lib/self.js')(xboxApiClient),
+        require('./lib/user.js')(xboxApiClient),
+        require('./lib/media.js')(xboxApiClient),
+        require('./lib/clubs.js')(xboxApiClient),
+        require('./lib/games.js')(xboxApiClient),
+        require('./lib/marketplace.js')(xboxApiClient));
 }
